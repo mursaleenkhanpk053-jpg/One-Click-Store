e28dd54eb14246301fa88d022394486b1d10ae0d
mursaleenkhanpk053-jpg <mursaleenkhanpk053@gmail.com>
2026-07-16 22:57:48 +0500

    Create product.js

---
+++ product.js
@@ -0,0 +1,46 @@
+const products = [
+
+{
+    id: 1,
+    name: "Microfiber Cleaning Cloth",
+    category: "Car Accessories",
+    price: 400,
+    image: "logo.png",
+    rating: "⭐⭐⭐⭐⭐",
+    description: "Super absorbent microfiber towel for car cleaning."
+},
+
+
+{
+    id: 2,
+    name: "Radiator Coolant",
+    category: "Automotive",
+    price: 550,
+    image: "logo.png",
+    rating: "⭐⭐⭐⭐",
+    description: "Heavy duty anti rust radiator coolant."
+},
+
+
+{
+    id: 3,
+    name: "Toyota Corolla Fuel Pump",
+    category: "Automotive",
+    price: 2250,
+    image: "logo.png",
+    rating: "⭐⭐⭐⭐⭐",
+    description: "Fuel pump assembly for Toyota Corolla."
+},
+
+
+{
+    id: 4,
+    name: "Beauty Product",
+    category: "Beauty",
+    price: 999,
+    image: "logo.png",
+    rating: "⭐⭐⭐⭐",
+    description: "Quality beauty product."
+}
+
+];
