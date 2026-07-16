1df987ff29d1c709a3a9d52e3492f8f617ea797d
mursaleenkhanpk053-jpg <mursaleenkhanpk053@gmail.com>
2026-07-16 22:55:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
-document.querySelectorAll('button').forEach(btn=>{
-    btn.addEventListener('click',()=>{
-        alert('Product added to cart!');
+// Cart System
+
+let cart = 0;
+
+
+// Add To Cart Function
+
+function addToCart(){
+
+    cart++;
+
+    document.getElementById("cartCount").innerHTML = cart;
+
+    alert("Product added to cart!");
+
+}
+
+
+
+
+// Search Function
+
+function searchProduct(){
+
+    let input = document.getElementById("searchInput").value.toLowerCase();
+
+    let products = document.querySelectorAll(".product-card");
+
+
+    products.forEach(function(product){
+
+        let name = product.querySelector("h3").innerText.toLowerCase();
+
+
+        if(name.includes(input)){
+
+            product.style.display="block";
+
+        }
+
+        else{
+
+            product.style.display="none";
+
+        }
+
+
     });
-});
+
+
+}
+
+
+
+// Shop Button
+
+document.querySelector(".hero button").onclick=function(){
+
+    window.scrollTo({
+
+        top:700,
+
+        behavior:"smooth"
+
+    });
+
+};
